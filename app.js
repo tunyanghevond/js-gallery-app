0dad2046fdaf75638e62e4813ddd9bbc8ad86dd2
tunyanghevond <83297507+tunyanghevond@users.noreply.github.com>
2022-06-17 17:20:06 +0200

    Update app.js
    Add gallery constructor function

--- app.js
+++ app.js
@@ -7,3 +7,33 @@ function getElement(selection) {
     `Please check "${selection}" selector, no such element exists`
   );
 }
+function Gallery(element) {
+  this.container = element;
+  this.list = [...element.querySelectorAll('.img')];
+  // target
+  this.modal = getElement('.modal');
+  this.modalImg = getElement('.main-img');
+  this.imageName = getElement('.image-name');
+  this.modalImages = getElement('.modal-images');
+  this.closeBtn = getElement('.close-btn');
+  this.nextBtn = getElement('.next-btn');
+  this.prevBtn = getElement('.prev-btn');
+  // self ref
+  // let self = this;
+  // bind functions
+  // this.openModal = this.openModal.bind(this);
+  this.closeModal = this.closeModal.bind(this);
+  this.nextImage = this.nextImage.bind(this);
+  this.prevImage = this.prevImage.bind(this);
+  this.chooseImage = this.chooseImage.bind(this);
+  // container event
+  this.container.addEventListener(
+    'click',
+    function (e) {
+      // self.openModal();
+      if (e.target.classList.contains('img')) {
+        this.openModal(e.target, this.list);
+      }
+    }.bind(this)
+  );
+}
